a855cd7f41fe817b38e167362ff20dc3a57c1dc4
Julio Farah <julio.farah@gmail.com>
2024-02-26 10:22:05 -0800

    WIP sidenav (#3)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,8 @@ import { Providers } from './providers';
 import { Noto_Sans } from 'next/font/google'
 import { Sidenav } from './components/sidenav'
 import './styles/base.css'
+import Head from 'next/head'
+import { Header } from './components/header'
 
 const noto = Noto_Sans({
   subsets: ['latin'],
@@ -13,9 +15,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en" className="h-full">
-        <body className={`flex w-full h-full overflow-hidden bg-white ${noto.className}`}>
-          <div className="flex flex-col h-full w-full">
-            <Sidenav />
+
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+       <link
+          rel="icon"
+          key="icon"
+          href={'/favicon.ico'}
+        />
+    </Head>
+
+        <body className={`w-full h-full overflow-hidden bg-white ${noto.className}`}>
+          <div className="h-full w-full">
+            <div className="flex flex-row">
+              <Header />
+              <Sidenav />
+            </div>
             <Providers>{children}</Providers>
           </div>
         </body>
